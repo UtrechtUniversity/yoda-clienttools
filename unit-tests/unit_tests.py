4e0237a7f2620b385c8d2017e418ca513a60bdfe
claravox <claravox@users.noreply.github.com>
2025-08-12 08:36:05 +0200

    Remove deprecated makeSuite

--- unit-tests/unit_tests.py
+++ unit-tests/unit_tests.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-__copyright__ = 'Copyright (c) 2019-2024, Utrecht University'
+__copyright__ = 'Copyright (c) 2019-2025, Utrecht University'
 __license__   = 'GPLv3, see LICENSE'
 
-from unittest import makeSuite, TestSuite
+from unittest import TestSuite
 
 from test_importgroups import ImportGroupsTest
 from test_reportoldvsnewdata import OldNewDataReportTest
@@ -12,7 +12,8 @@ from test_yoda_names import YodaNamesTest
 
 def suite():
     test_suite = TestSuite()
-    test_suite.addTest(makeSuite(ImportGroupsTest))
-    test_suite.addTest(makeSuite(OldNewDataReportTest))
-    test_suite.addTest(makeSuite(YodaNamesTest))
+    test_suite.addTest(ImportGroupsTest)
+    # test_suite.addTest(ExportGroupsTest)
+    test_suite.addTest(OldNewDataReportTest)
+    test_suite.addTest(YodaNamesTest)
     return test_suite
